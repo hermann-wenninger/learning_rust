782dddc2b71b645707930bbea18d5c18a25fadfb
hermann wenninger <info@wennhmann.de>
2024-02-25 18:24:50 +0100

    repair startup

--- cap_0/target/debug/deps/cap_0-01c6b37902dc96ab.d
+++
@@ -1,7 +0,0 @@
-D:\a_work\learning_rust\cap_0\target\debug\deps\libcap_0-01c6b37902dc96ab.rmeta: src\main.rs src\lex.rs src\expr.rs
-
-D:\a_work\learning_rust\cap_0\target\debug\deps\cap_0-01c6b37902dc96ab.d: src\main.rs src\lex.rs src\expr.rs
-
-src\main.rs:
-src\lex.rs:
-src\expr.rs:
