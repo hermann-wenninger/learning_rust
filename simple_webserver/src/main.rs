b34ccae5eb7d428f0f95f96bb3c306a1e532f8e8
hermann wenninger <info@wennhmann.de>
2024-02-28 14:34:10 +0100

    index in deps

--- simple_webserver/src/main.rs
+++ simple_webserver/src/main.rs
@@ -1,10 +1,11 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8077").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -21,5 +22,12 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
