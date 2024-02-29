3781e4b9fd9451837612da62dc8c30f6174f8fea
hermann wenninger <info@wennhmann.de>
2024-02-29 12:52:38 +0100

    guessing zero

--- cap_0/src/main.rs
+++ cap_0/src/main.rs
@@ -2,6 +2,15 @@
 //! ```
 //! println!("Hello Wolrd and universe");
 //! ```
+
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Rate eien zahl zwischen 0 und 100");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("fehler beim lesen");
+    println("SCHÄTZUNG ... {guess}")
+
 }
