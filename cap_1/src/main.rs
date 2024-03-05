5c957e167a9a58560cb2d2b8af730436f880c184
hermann wenninger <info@wennhmann.de>
2024-03-05 17:35:26 +0100

    borrowing and reihenfolgen

--- cap_1/src/main.rs
+++ cap_1/src/main.rs
@@ -1,3 +1,8 @@
+fn ein_tupel(str:String)->(String, usize){
+    let laenge = str.len();
+    (str, laenge)
+}
+
 fn gib_ownership()->String{
     let ein_string = String::from("ich komme aus einer funktion");
     ein_string
@@ -8,7 +13,11 @@ fn take_literal(lit:u8){
 fn take_literalx(litx:&String){
     println!("i am in a funktion --->{}",litx)
 }
+fn nimmt_und_gibt_zurück(ein_str: String) -> String { // a_string kommt in den
+    ein_str  
+}
 fn main() {
+    let zu_und_zurück = String::from("ich wandere von einem zum andern");
     let s = String::from("ich lande auf dem heap");
     let x = 21;
     let literal = "literal".to_string();
@@ -17,5 +26,11 @@ fn main() {
     take_literal(x);
     println!("und danach lande ich nochmal---->{}",x);
     take_literalx(&literal);
-    println!("nochmal--->{literal}")
+    println!("nochmal--->{literal}");
+    let wanderer = nimmt_und_gibt_zurück(zu_und_zurück);
+    println!("ich bin ein makro und habe die eigentümerschaft von: {}",wanderer);
+    println!("ich bin ein makro und habe die eigentümerschaft von: {}",wanderer);
+    println!("ich bin ein makro und habe die eigentümerschaft von: {}",wanderer);
+    let pull =  ein_tupel(wanderer);
+    println!("{} ---> und habe die länge {}",pull.0, pull.1);
 }
