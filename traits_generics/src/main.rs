12af71b80b2702c43d94582cf305ffc29e484aac
hermann wenninger <info@wennhmann.de>
2024-03-09 20:12:36 +0100

    generics trait

---
+++ traits_generics/src/main.rs
@@ -0,0 +1,18 @@
+// traits and generics for structs enums tupels and functions
+// struct Foo <T>{
+//    field : T,
+//                }
+// enum Fool <T>{
+//    Variante (T),
+//  
+//            }
+// fn function<T>(val: T){}
+//
+//   struct Flusn <T>(T) ;   
+//
+//  impl<T> Floss<T> {}      
+
+
+fn main() {
+    println!("Hello, world!");
+}
