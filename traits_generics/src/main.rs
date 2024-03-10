8446c2591592d944742f28946778a6764d3c6196
hermann wenninger <info@wennhmann.de>
2024-03-10 18:05:37 +0100

    trait as interface

--- traits_generics/src/main.rs
+++ traits_generics/src/main.rs
@@ -10,9 +10,56 @@
 //
 //   struct Flusn <T>(T) ;   
 //
-//  impl<T> Floss<T> {}      
+//  impl<T> Floss<T> {}  
 
+ enum Result <T, E> {
+        Some(T),
+        Error(E),
+    }
 
-fn main() {
-    println!("Hello, world!");
+struct Option<T>{
+    Value: T,
 }
+impl<T> Option<T>{
+    fn ok_or_error<E>(self error: E)->Result<T, E>{
+        match self{
+            Some(t) => Ok(t),
+            None   => Err(err),
+        }
+    }
+}
+let size = size_of::<i32>();
+
+fn parse<T>(s:&str)-> Result<T,()>{
+    unimplementet!();
+}
+let x = "42".parse::<i32>();
+let x ="42".parse().unwrap();
+let x:Result<i32,_> = "322".parse();
+
+fn makeit(bar:i32)->Result<i32,_>{
+    let x ="42".parse();
+    match x{
+        Ok(ref mut x) => x + bar,
+        None => {},
+    }
+fn min<T>(a:T, b:T)->T{
+    where T: PartialOrd,
+    {
+        if a<b{a} else{b}
+    }
+}
+//ODER
+fn min<T:PartialOrd>(a:T, b:T){}
+}
+trait Goon{
+ fn go_on(&self);
+}
+struct Car{name:String};
+impl Goon for Car{
+    fn go_on(&self){
+        println!("wieder ein schritt weiter")
+    }
+}
+let vw = Car{name:"volkswagen".into()};
+vw.go_on();
